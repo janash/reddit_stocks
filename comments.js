203c02b75dcc24018da86c0be4d04156c2524567
GitHub Action <action@github.com>
2021-01-26 14:57:48 +0000

    Update index for Tue Jan 26 14:57:48 UTC 2021

--- comments.js
+++ comments.js
@@ -50,7 +50,7 @@ $(document).ready( function (){
         
 
         for (let i=0; i<subreddits.length; i++) {
-            let file_name = `https://raw.githubusercontent.com/janash/reddit_stocks/data/daily_data/2021-01-24_${subreddits[i]}_comments.csv`
+            let file_name = `https://raw.githubusercontent.com/janash/reddit_stocks/data/daily_data/2021-01-26_${subreddits[i]}_comments.csv`
             
             let subComments = load_file(file_name)
             let processedComments = tickerArray(subComments, subreddits[i], ticker)
@@ -58,7 +58,7 @@ $(document).ready( function (){
         }
     }
     else {
-        let file_name = `https://raw.githubusercontent.com/janash/reddit_stocks/data/daily_data/2021-01-24_${subreddit}_comments.csv`
+        let file_name = `https://raw.githubusercontent.com/janash/reddit_stocks/data/daily_data/2021-01-26_${subreddit}_comments.csv`
         let subComments = load_file(file_name)
         let processedComments = tickerArray(subComments, subreddit, ticker)
         comments = processedComments
